c1b902469246fc2083292c3c50f1b15fba045127
DuyKhanh <duykhanhlb4999@gmail.com>
2020-07-26 20:13:33 +0700

    update

--- routes/schedule.route.js
+++ routes/schedule.route.js
@@ -1,18 +1,17 @@
-var express=require('express')
+var express = require("express");
 
-var router=express.Router()
+var router = express.Router();
 
-var controller=require('../controller/schedule.controller')
+var controller = require("../controller/schedule.controller");
 
+router.get("/create", controller.createSchedule);
 
-router.get('/create', controller.createSchedule)
+router.post("/PostData", controller.schedulePostData);
 
+router.put("/updateReserved", controller.updateReserved);
 
-router.post('/PostData', controller.schedulePostData)
+router.get("/data", controller.getScheduleData);
 
+router.get("/data/:id", controller.getScheduleDataByID);
 
-router.get("/data", controller.getScheduleData)
-
-router.get("/data/:id", controller.getScheduleDataByID)
-
-module.exports=router
+module.exports = router;
